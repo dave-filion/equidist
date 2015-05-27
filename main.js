2b9c32b7764538a49e63d970b07a99d41927162b
Dave Filion <dave.filion@gmail.com>
2015-05-26 23:20:55 -0400

    screwing around with events

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ var https = require('https');
 var querystring = require('querystring');
 var fs = require('fs');
 var path = require('path');
+var events = require('events');
+
+var actions = {
+  PARSE_JSON: 'parse_json'
+};
 
 var apiKeyFilePath = path.join(__dirname, 'api.key'),
     apiKey = fs.readFileSync(apiKeyFilePath, {encoding: 'utf-8'}).trim();
 
+var jsonParseStream = new events.EventEmitter();
+jsonParseStream.on(actions.PARSE_JSON, function(string) {
+  console.log(string);
+});
+
 // mode can be walking / bicycling / transit
-function getDirections(start, end, mode) {
+function getDirections(start, end, mode, out) {
   var query = querystring.stringify({
     key: apiKey,
     origin: start,
@@ -21,11 +31,11 @@ function getDirections(start, end, mode) {
     console.log('STATUS: ' + res.statusCode);
     res.setEncoding('utf8');
     res.on('data', function(chunk) {
-      console.log('BODY: ' + chunk);
+      out.emit(actions.PARSE_JSON, chunk);
     });
   }).on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
 }
 
-getDirections('262 10th st brooklyn ny', '163 varick st new york NY', 'transit');
+getDirections('262 10th st brooklyn ny', '163 varick st new york NY', 'transit', jsonParseStream);
